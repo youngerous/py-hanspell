8d64f80a69a2b75a90890dce81cca48833a53d5a
youngerous <youngerous@gmail.com>
2020-04-29 20:25:27 +0900

    fix api url

--- hanspell/spell_checker.py
+++ hanspell/spell_checker.py
@@ -50,7 +50,8 @@ def check(text):
     }
 
     headers = {
-        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+        'user-agent':'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36',
+        'referer': 'https://search.naver.com/' 
     }
 
 
